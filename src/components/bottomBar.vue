925ce2581476ed52fdb3ce980a84ccbb50563f37
mathieu-anderson <mathieu.hj.anderson@gmail.com>
2017-02-26 18:10:05 +1300

    Changed icons, added Home link

--- src/components/bottomBar.vue
+++ src/components/bottomBar.vue
@@ -1,9 +1,11 @@
 <template lang="html">
   <div class="bottomBar">
     <md-bottom-bar>
-      <md-bottom-bar-item md-iconset="ion-android-happy" @click.native="goAbout">
+      <md-bottom-bar-item md-iconset="ion-ios-home-outline" @click.native="goHome">
       </md-bottom-bar-item>
-      <md-bottom-bar-item md-iconset="ion-android-mail" @click.native="goContact">
+      <md-bottom-bar-item md-iconset="ion-ios-information-outline" @click.native="goAbout">
+      </md-bottom-bar-item>
+      <md-bottom-bar-item md-iconset="ion-ios-paperplane-outline" @click.native="goContact">
       </md-bottom-bar-item>
     </md-bottom-bar>
   </div>
@@ -17,6 +19,9 @@ export default {
     }
   },
   methods: {
+    goHome () {
+      this.$router.push('/')
+    },
     goAbout () {
       this.$router.push('/about-me')
     },
